ab294489bce55b809db428dc52f5f82bdf307d78
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-12-27 21:27:28 +0100

    added more files

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-"use strict"
+// "use strict"
 
 
-function navcontent(contentId) {
-    var contents = document.querySelectorAll('.content');
-    contents.forEach(function(content) {
-        content.style.display = 'none';
-    });
+// function navcontent(contentId) {
+//     var contents = document.querySelectorAll('.content');
+//     contents.forEach(function(content) {
+//         content.style.display = 'none';
+//     });
 
-    var selectedContent = document.getElementById(contentId);
-    selectedContent.style.display = 'block';
-}
+//     var selectedContent = document.getElementById(contentId);
+//     selectedContent.style.display = 'block';
+// }
