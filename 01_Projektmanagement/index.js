80c196c2d2b92acfd58bf90ec0ab5bdf817d17e8
Yalcin Kurt <yalcin.kurt89@gmail.com>
2024-02-20 16:42:02 +0100

    update pm

---
+++ 01_Projektmanagement/index.js
@@ -0,0 +1,39 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+
+      // Stroked triangle
+      ctx.beginPath();
+      ctx.moveTo(300, 200);
+      ctx.lineTo(100, 200);
+      ctx.lineTo(200, 100);
+      ctx.closePath();
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(140, 210);
+      ctx.lineTo(140, 240);
+      ctx.lineTo(0, 240);
+      ctx.lineTo(0, 210);
+      ctx.lineTo(140, 210);
+      ctx.closePath();
+      ctx.font = "25px arial";
+      ctx.fillText("Zeit", 50, 235);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(400, 210);
+      ctx.lineTo(400, 240);
+      ctx.lineTo(260, 240);
+      ctx.lineTo(260, 210);
+      ctx.lineTo(400, 210);
+      ctx.closePath();
+      ctx.font = "25px arial";
+      ctx.fillText("Kosten", 295, 235);
+      ctx.stroke();
+
+    }
+  }
+  
+  draw();
